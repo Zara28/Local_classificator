a1313fe2f85976f83bac860ef9cf356d6db30bfd
Zara28 <developer1@mcrt.space>
2026-08-14 11:47:01 +0400

    тестирование на более сложном датасете

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 
-from test_data import raw_data
-
-df = pd.DataFrame(raw_data, columns=['text', 'category'])
+df = pd.read_csv("test_dataset.csv")
 
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer(
@@ -17,29 +15,28 @@ pipeline = Pipeline([
     ('classifier', LogisticRegression(random_state=42, C=1.0))
 ])
 
+df = df.sample(frac=1, random_state=42)
+
+df_test = df.iloc[:len(df) // 2]
+df_validation = df.iloc[len(df) // 2:]
+
 print("Обучение модели...")
-pipeline.fit(df['text'], df['category'])
+pipeline.fit(df_test['text'], df_test['category'])
 print("Обучение завершено\n")
 
-predictions = pipeline.predict(df['text'])
+predictions = pipeline.predict(df_test['text'])
 print("Отчет по качеству классификации:")
-print(classification_report(df['category'], predictions))
-
-test_requests = [
-    "Нужно написать SQL скрипт для расчета конверсии по дням",
-    "Ищу человека, который нарисует красивые иконки",
-    "Требуется бэкенд на python и база данных",
-    "Нужен спец по закупке рекламы в телеграм-каналах",
-    "Срочно починить вылеты на айфоне"
-]
+print(classification_report(df_test['category'], predictions))
 
 print("-" * 40)
 print("Тестирование на новых заявках:\n")
-for req in test_requests:
-    predicted_category = pipeline.predict([req])[0]
+texts = df_validation['text']
+predictions = pipeline.predict(texts)
+probabilities = pipeline.predict_proba(texts)
 
-    probabilities = pipeline.predict_proba([req])[0]
-    max_prob = max(probabilities) * 100
+# 2. Итерируемся по текстам и уже готовым результатам с помощью zip
+for req, pred, probs in zip(texts, predictions, probabilities):
+    max_prob = max(probs) * 100
 
     print(f"ТЗ: «{req}»")
-    print(f"Категория: {predicted_category} (уверенность: {max_prob:.1f}%)\n")
+    print(f"Категория: {pred} (уверенность: {max_prob:.1f}%)\n")
