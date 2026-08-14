2c3fce24e57b3bbf8b62b17da1c9be6bef90bebb
Zara28 <developer1@mcrt.space>
2026-08-14 12:15:23 +0400

    оформление решения и гитигнор

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
+from TaskClassifier import TaskClassifier
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sentence_transformers import SentenceTransformer
 
-print("Загрузка модели rubert-tiny2...")
-embedder = SentenceTransformer('cointegrated/rubert-tiny2')
+if __name__ == "__main__":
+    df = pd.read_csv("test_dataset.csv")
 
-# 1. Загружаем датасет
-df = pd.read_csv("test_dataset.csv")
+    classifier = TaskClassifier()
+    classifier.train(texts=df['text'].tolist(), labels=df['category'].tolist())
 
-df_train, df_test = train_test_split(
-    df,
-    test_size=0.5,
-    random_state=42,
-    stratify=df['category']
-)
+    new_requests = [
+        "Нужен человек для отрисовки графики на канал",
+        "Требуется переписать базу с MS SQL на постгрес и настроить бэкапы",
+        "Ищем специалиста для анализа метрик оттока из приложения",
+        "Надо запустить рекламу в телеге на широкую аудиторию"
+    ]
 
-train_labels = df_train['category'].tolist()
-test_labels = df_test['category'].tolist()
-
-print("Векторизация обучающих данных...")
-X_train = embedder.encode(df_train['text'].tolist())
-
-print("Векторизация тестовых данных...")
-X_test = embedder.encode(df_test['text'].tolist())
-
-print("Обучение классификатора...\n")
-clf = LogisticRegression(random_state=42, C=10.0, max_iter=1000)
-clf.fit(X_train, train_labels)
-
-predictions = clf.predict(X_test)
-print("Отчет по качеству:")
-print(classification_report(test_labels, predictions))
+    for req in new_requests:
+        result = classifier.predict(req)
+        print(f"Входящее ТЗ: «{result['text']}»")
+        print(f"Категория:   {result['category']}")
+        print(f"Уверенность: {result['confidence']}%\n")
